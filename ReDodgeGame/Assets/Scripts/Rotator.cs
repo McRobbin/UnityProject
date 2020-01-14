b41ea7dfee7f96e5a6a5dd8e35d85ce99ca366e9
McRobbin <skagus1247@naver.com>
2020-01-14 16:52:42 +0900

    유니티 프로젝트

---
+++ ReDodgeGame/Assets/Scripts/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotationSpeed = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.deltaTime;
+
+        transform.Rotate(0f, rotationSpeed * time, 0f);
+    }
+}
